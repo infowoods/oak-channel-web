373d3e8e16c6a4d7bec3d911464ff3fd40169231
xingchi <starunbian@foxmail.com>
2022-07-16 01:24:04 +0800

    add recommand

--- components/TopBar/index.js
+++ components/TopBar/index.js
@@ -1,17 +1,44 @@
+import { useState } from 'react'
+import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import dynamic from 'next/dynamic'
+
 import Icon from '../../widgets/Icon'
+const BottomSheet = dynamic(() => import('../../widgets/BottomSheet'))
+
+import storageUtil from '../../utils/storageUtil'
+
 import styles from './index.module.scss'
+import favImg from '../../public/favicon.png'
 
 function TopBar(props) {
   const { url } = props
+  const { t } = useTranslation('common')
   const router = useRouter()
-  console.log(url)
+  const [show, setShow] = useState(false)
+
+  const botList = {
+    owl: {
+      id: '60d30faa-3825-48da-ac1c-6aca1f573dd5',
+      url: 'https://mixin.owldeliver.one',
+    },
+  }
+
+  const handleClose = () => setShow(false)
+
+  const handleBotClick = (name) => {
+    setShow(false)
+    if (storageUtil.get('platform') === 'browser') {
+      window.open(botList[name].url)
+    } else {
+      window.open(`mixin://apps/${botList[name].id}`)
+    }
+  }
 
   return (
     <div className={styles.bar}>
-      {
-        url &&
+      {url && (
         <Icon
           className={styles.back}
           type="arrow-right"
@@ -19,16 +46,41 @@ function TopBar(props) {
             router.push(url)
           }}
         />
-      }
-      <div className={`${styles.icon} ${url && styles.iconPadding}`}>
-        <Image
-          src="/favicon.png"
-          alt="favico"
-          width={28}
-          height={28}
-        />
+      )}
+      <div
+        className={`${styles.icon} ${url && styles.iconPadding}`}
+        onClick={() => setShow(true)}
+      >
+        <Image src={favImg} alt="favico" width={28} height={28} />
         <span>Oak Topic Hub</span>
+        <Icon
+          type="arrow-right"
+          className={show ? styles.active : styles.inactive}
+        />
       </div>
+
+      <BottomSheet
+        t={t}
+        show={show}
+        onClose={handleClose}
+        onCancel={handleClose}
+      >
+        <div className={styles.botWrap}>
+          <div className={styles.bot} onClick={() => handleBotClick('owl')}>
+            <Image
+              src={favImg}
+              alt="owl deliver"
+              width={38}
+              height={38}
+              layout="fixed"
+            />
+            <div>
+              <p>Owl Deliver</p>
+              <p>Subceibe all kinds of feeds</p>
+            </div>
+          </div>
+        </div>
+      </BottomSheet>
     </div>
   )
 }
