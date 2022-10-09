38f52b204b563689bc4e1ae5f190c5e7b0f84d83
NodeWee <nodewee@gmail.com>
2022-10-09 21:34:21 +0800

    update top bar

--- components/TopBar/index.js
+++ components/TopBar/index.js
@@ -1,87 +1,82 @@
 import { useState } from 'react'
-import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
-import dynamic from 'next/dynamic'
 
-import Icon from '../../widgets/Icon'
-const BottomSheet = dynamic(() => import('../../widgets/BottomSheet'))
+import { RiArrowLeftLine, RiArrowDownSLine } from 'react-icons/ri'
+import { toLogin } from '../../utils/loginUtil'
+import Avatar from '../../widgets/Avatar'
 
-import storageUtil from '../../utils/storageUtil'
+import { APPS } from '../../constants'
+import favIconImg from '../../public/favicon.png'
 
 import styles from './index.module.scss'
-import favImg from '../../public/favicon.png'
-import favImgOwl from '../../public/favicon-owl.png'
 
 function TopBar(props) {
-  const { url } = props
-  const { t } = useTranslation('common')
-  const router = useRouter()
-  const [show, setShow] = useState(false)
+  const { ctx, t, curLogin, backPath, setShowApps } = props
 
-  const botList = {
-    owl: {
-      id: '60d30faa-3825-48da-ac1c-6aca1f573dd5',
-      url: 'https://mixin.owldeliver.one',
-    },
+  const router = useRouter()
+
+  const avatarLink = (path) => {
+    switch (path) {
+      case '/user':
+        break
+      default:
+        return '/user'
+    }
   }
 
-  const handleClose = () => setShow(false)
-
-  const handleBotClick = (name) => {
-    setShow(false)
-    if (storageUtil.get('platform') === 'browser') {
-      window.open(botList[name].url)
+  const handleAvatarClick = () => {
+    const link = avatarLink(router.pathname)
+    if (link) {
+      router.push(avatarLink(router.pathname))
     } else {
-      window.open(`mixin://apps/${botList[name].id}`)
+      return
     }
   }
 
   return (
     <div className={styles.bar}>
-      {url && (
-        <Icon
-          className={styles.back}
-          type="arrow-right"
-          onClick={() => {
-            router.push(url)
-          }}
-        />
-      )}
-      <div
-        className={`${styles.icon} ${url && styles.iconPadding}`}
-        onClick={() => setShow(true)}
-      >
-        <Image src={favImg} alt="favico" width={28} height={28} />
-        <span>Oak Topic Hub</span>
-        <Icon
-          type="arrow-right"
-          className={show ? styles.active : styles.inactive}
-        />
+      <div className={styles.left}>
+        {backPath && (
+          <RiArrowLeftLine
+            className={styles.back}
+            onClick={() => {
+              router.push(backPath)
+            }}
+          />
+        )}
+
+        {!backPath && curLogin?.user && (
+          <div className={styles.avatar}>
+            <Avatar
+              isGroup={curLogin?.group?.is_group}
+              imgSrc={curLogin?.user?.avatar}
+              onClick={() => handleAvatarClick()}
+            />
+          </div>
+        )}
+        {!backPath && !curLogin?.user && (
+          <div className={styles.login} onClick={() => toLogin()}>
+            <span>
+              {curLogin?.group?.is_group ? t('owner_login') : t('login')}
+            </span>
+          </div>
+        )}
       </div>
 
-      <BottomSheet
-        t={t}
-        show={show}
-        onClose={handleClose}
-        onCancel={handleClose}
-      >
-        <div className={styles.botWrap}>
-          <div className={styles.bot} onClick={() => handleBotClick('owl')}>
-            <Image
-              src={favImgOwl}
-              alt="owl deliver"
-              width={38}
-              height={38}
-              layout="fixed"
-            />
-            <div>
-              <p>Owl Deliver</p>
-              <p>Subscribe all kinds of feeds</p>
-            </div>
-          </div>
+      <div className={styles.middle}>
+        <div
+          className={styles.app}
+          onClick={() => {
+            setShowApps(true)
+          }}
+        >
+          <Image src={favIconImg} alt="favicon" width={24} height={24} />
+          <span className={styles.title}>{t(APPS.oak.title)}</span>
+          <RiArrowDownSLine className={styles.arrow} />
         </div>
-      </BottomSheet>
+      </div>
+      <div className={styles.right}></div>
     </div>
   )
 }
--- widgets/Avatar/index.js
+++ widgets/Avatar/index.js
@@ -1,20 +1,15 @@
 import Image from 'next/image'
-import styles from './index.module.scss'
 
 function Avatar(props) {
-  const {
-    imgSrc,
-    ...others
-  } = props
-  const defalutAvatar = '/default-avatar.png'
+  const { isGroup = false, imgSrc, ...others } = props
+  const defaultAvatar = '/default-avatar.png'
 
   return (
     <Image
-      src={imgSrc ? imgSrc : defalutAvatar}
+      src={isGroup ? defaultAvatar : imgSrc ? imgSrc : defaultAvatar}
       alt="avatar"
-      width={36}
-      height={36}
-      className={styles.avatar}
+      width={28}
+      height={28}
       {...others}
     />
   )
