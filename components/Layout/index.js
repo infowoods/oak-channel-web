6a8bf32689f44f8f91c9799f550a023cd288032d
NodeWee <nodewee@gmail.com>
2022-10-09 22:57:46 +0800

    update layout

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,142 +1,127 @@
 import { useState, useEffect, useContext } from 'react'
-import { i18n, useTranslation } from 'next-i18next'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
-
-import { ProfileContext } from '../../stores/useProfile'
-
+import { i18n, useTranslation } from 'next-i18next'
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
 import TopBar from '../TopBar'
-import Avatar from '../../widgets/Avatar'
-import BottomNav from '../../widgets/BottomNav'
 import Loading from '../../widgets/Loading'
+import BottomNav from '../../widgets/BottomNav'
 
-import { getMixinContext, reloadTheme } from '../../services/api/mixin'
+import { CurrentLoginContext } from '../../contexts/currentLogin'
+import { getMixinContext, reloadTheme } from '../../utils/pageUtil'
+import { APPS } from '../../constants'
+import { loadToken, loadUserData, loadGroupData } from '../../utils/loginUtil'
 
-import { authLogin } from '../../utils/loginUtil'
-import storageUtil from '../../utils/storageUtil'
+const AppsJumper = dynamic(() => import('./AppsJumper'))
 
 import styles from './index.module.scss'
 
 function Layout({ children }) {
   const { t } = useTranslation('common')
-  const [state, dispatch] = useContext(ProfileContext)
-  const { pathname, push } = useRouter()
-  const [theme, setTheme] = useState('')
-  const [platform, setPlatform] = useState(false)
-  const isLogin = state.isLogin
-
+  const router = useRouter()
+  const [curLogin, _] = useContext(CurrentLoginContext)
+  const [ctx, setCtx] = useState({})
+  const [showApps, setShowApps] = useState(false)
+  const [barColor, setBarColor] = useState('#999999')
   const navHref = ['/', '/user']
 
-  const getBarColor = (path) => {
-    reloadTheme(platform)
-    if (theme === 'dark') {
-      return path.includes('/oak/') ? '#1E1E1E' : '#080808'
-    }
-    return path.includes('/oak/') ? '#F4F6F7' : '#FFFFFF'
-  }
-
-  const backLink = (path) => {
-    if (path.includes('oak/')) {
-      return '/'
-    }
-  }
-
-  const avatarLink = (path) => {
-    switch (path) {
-      default:
-        return
-    }
-  }
-
-  const handleClick = () => {
-    const link = avatarLink(pathname)
-    if (link) {
-      push(avatarLink(pathname))
-    } else {
-      return
+  const getBackPath = (curPath) => {
+    if (curPath.startsWith('/user/') && curPath.length > 6) {
+      return '/user'
     }
   }
 
   useEffect(() => {
-    console.log('>>> layout init:', pathname)
     const ctx = getMixinContext()
-    ctx.appearance &&
-      document.documentElement.setAttribute('data-theme', ctx.appearance)
-    setTheme(ctx.appearance || 'light')
+    setCtx(ctx)
+
+    const updateTheme = () => {
+      if (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      ) {
+        const clr = '#080808'
+        setBarColor(clr)
+        document
+          .querySelector("meta[name='theme-color']")
+          .setAttribute('content', clr)
+      } else {
+        const clr = '#FFFFFF'
+        setBarColor(clr)
+        document
+          .querySelector("meta[name='theme-color']")
+          .setAttribute('content', clr)
+      }
+      reloadTheme()
+    }
+    updateTheme()
+    // To watch for theme changes:
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (event) => {
+        updateTheme()
+      })
 
     if (
       ctx?.locale &&
       ctx.locale !== 'zh-CN' &&
       i18n.language !== 'en' &&
-      pathname !== '/callback/mixin'
+      router.pathname !== '/callback/mixin'
     ) {
       i18n.changeLanguage('en')
-      push(pathname, pathname, { locale: 'en' })
+      router.push(router.pathname, router.pathname, { locale: 'en' })
       return
     }
 
-    if (!ctx?.app_version) {
-      storageUtil.set('platform', 'browser')
+    if (router.pathname !== '/callback/mixin') {
+      curLogin.token = loadToken(ctx?.conversation_id)
+      curLogin.user = loadUserData(ctx?.conversation_id)
+      curLogin.group = loadGroupData(ctx?.conversation_id)
     }
-    ctx?.platform && setPlatform(ctx?.platform)
 
-    console.log('>>> localStorage: ', storageUtil.get('user_info'))
-    storageUtil.get('user_info') &&
-      dispatch({
-        type: 'userInfo',
-        userInfo: storageUtil.get('user_info'),
-      })
-    dispatch({
-      type: 'isLogin',
-      isLogin: storageUtil.get('user_info')?.user_name ? true : false,
-    })
+    reloadTheme()
   }, [])
 
   return (
-    <div
-      className={`${styles.wrap} ${
-        pathname.includes('/oak/') && styles.bgGray
-      }`}
-    >
-      <Head>
-        <title>Oak Topic Hub</title>
-        <meta name="description" content="Oak Topic Hub" />
-        <meta name="theme-color" content={getBarColor(pathname)} />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+    <>
+      <div className={styles.wrap}>
+        <Head>
+          <title>{t(APPS.oak.title)}</title>
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1, maximum-scale=1, user-scalable=no"
+          />
+          <meta name="description" content={t(APPS.oak.description)} />
+          <meta name="theme-color" content={barColor} />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
 
-      {pathname !== '/callback/mixin' ? (
-        <>
-          <TopBar url={backLink(pathname)} />
-          <div className={styles.avatarWrap}>
-            <div>
-              {isLogin
-                ? navHref.includes(pathname) && (
-                    <div className={styles.avatar}>
-                      <Avatar
-                        imgSrc={state.userInfo?.avatar}
-                        onClick={handleClick}
-                      />
-                    </div>
-                  )
-                : isLogin === false && (
-                    <div className={styles.login} onClick={() => authLogin()}>
-                      <span>{t('login')}</span>
-                    </div>
-                  )}
-            </div>
-          </div>
-        </>
-      ) : (
-        <>
-          <Loading size={36} className={styles.loading} />
-        </>
-      )}
-      <div>
+        {router.pathname === '/callback/mixin' ? (
+          <>
+            <Loading size={36} className={styles.loading} />
+          </>
+        ) : (
+          <>
+            <TopBar
+              ctx={ctx}
+              t={t}
+              curLogin={curLogin}
+              backPath={getBackPath(router.pathname)}
+              showApps={showApps}
+              setShowApps={setShowApps}
+            />
+          </>
+        )}
+
         {children}
-        {navHref.includes(pathname) && <BottomNav t={t} />}
+
+        {navHref.includes(router.pathname) && (
+          <BottomNav ctx={ctx} t={t} curLogin={curLogin} />
+        )}
+
+        {showApps && <AppsJumper t={t} setShowApps={setShowApps} />}
       </div>
-    </div>
+    </>
   )
 }
 
