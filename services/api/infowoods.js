ea6d8874a98b5a36b7c6617c67796dc4fb46b735
NodeWee <nodewee@gmail.com>
2022-10-15 16:10:37 +0800

    update infowoods api

--- services/api/infowoods.js
+++ services/api/infowoods.js
@@ -84,55 +84,143 @@ export function oldVer_Unfollow(topic_id) {
 }
 
 // ==============================================
-// CHANNELS & COLLECTIONS
+// CHANNELS
 // ==============================================
 
 /**
+ * Get fee of create channel
+ *
+ */
+export function getFees_Channels() {
+  return http.get('/channels/fees')
+}
+
+export function createChannel({ title, description }) {
+  let data = {
+    title,
+    description,
+  }
+  return http.post('/channels', { data })
+}
+
+/**
+ * Parse Source URI
  *
  * @param {uri} String
  * @returns
- * 解析RSS地址
  */
-export function parseFeed(data) {
+export function parseSourceUri({ uri }) {
+  let data = { uri }
   return http.post('/channels/parse', { data })
 }
 
 /**
+ * Source Search
+ *
+ * @param {sourceType} String
+ * @param {text} String
+ * @param {limit} Number optional
+ * @param {continuous_key} String optional
+ * @returns
+ */
+export function sourceSearch({ sourceType, searchVal, limit, continuous_key }) {
+  let data = { source: sourceType, text: searchVal }
+  return http.post('/channels/search', { data })
+}
+
+/**
+ * Get Owned Channels
+ *
+ */
+export function getChannels() {
+  return http.get('/channels')
+}
+
+/**
+ * Read Channel Profile
+ *
+ * @param {channel_id} String
+ * @returns
+ */
+export function readChannel({ channel_id }) {
+  return http.get(`/channels/${channel_id}`)
+}
+
+/**
+ * Modify Channel Profile
+ *
+ * @param {channel_id} String
+ * @returns
+ */
+export function modifyChannel({
+  channel_id,
+  title,
+  description,
+  price_per_info,
+  searchable,
+}) {
+  let data = {
+    title,
+    description,
+    price_per_info,
+    searchable,
+  }
+
+  return http.put(`/channels/${channel_id}`, { data })
+}
+
+// ----------------------------------------
+// Publish Infos
+
+/**
+ * Get fees of infos
+ *
+ */
+export function getFees_Infos({ channel_id }) {
+  return http.get(`/channels/${channel_id}/infos/fees`)
+}
+
+/**
+ * Publish a text info to a channel
+ *
+ * @param {text} String
+ */
+export function publishTextInfo({ channel_id, text, title, source }) {
+  let info = { content_type: 'text', content_value: text }
+  if (title) info.title = title
+  if (source) info.source = source
+  let data = { infos: [info] }
+  return http.post(`/channels/${channel_id}/infos`, { data })
+}
+
+/**
+ * Publish a markdown info to a channel
+ *
+ * @param {text} String
+ */
+export function publishMarkdownInfo({ channel_id, text, title, source }) {
+  let info = { content_type: 'markdown', content_value: text }
+  if (title) info.title = title
+  if (source) info.source = source
+  let data = { infos: [info] }
+  return http.post(`/channels/${channel_id}/infos`, { data })
+}
+
+// ==============================================
+// COLLECTIONS
+// ==============================================
+
+/**
+ * Get collection
  *
  * @param {collection_id} String optional
  * @returns
- * get collection
  */
 export function getCollection(collection_id) {
   if (!collection_id) {
     throw 'collection_id is empty'
   }
   return http.get(`/collections/${collection_id}`)
-}
-
-/**
- *
- * @param {uri} String optional
- * @param {channel_id} String optional
- * @returns
- * 解析Channel地址
- */
-export function parseTopic(channel_id) {
-  return http.get(`/channels/${channel_id}`)
-}
-
-/**
- *
- * @param {source} String
- * @param {text} String
- * @param {limit} Number optional
- * @param {continuous_key} String optional
- * @returns
- * 搜索源
- */
-export function searchSource(searchType, searchVal) {
-  let data = { source: searchType, text: searchVal }
-  return http.post('/channels/search', { data })
 }
 
 // ==============================================
@@ -160,7 +248,7 @@ export function updateUserSettings(data) {
 }
 
 // ==============================================
-// TOP-UP
+// ORDERS
 // ==============================================
 
 /**
