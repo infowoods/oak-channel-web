1f7191412fe000e5c1b19e930d367c7a5f4943ee
NodeWee <nodewee@gmail.com>
2022-10-15 16:37:36 +0800

    add channel cards

---
+++ components/Home/ChannelCards/index.js
@@ -0,0 +1,119 @@
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
+import toast from 'react-hot-toast'
+import { Card, Row, Text, Grid } from '@nextui-org/react'
+
+import { TbTrees } from 'react-icons/tb'
+import {
+  RiMoneyDollarCircleLine,
+  RiUserFollowLine,
+  RiSearchLine,
+  RiCloseLine,
+  RiCheckLine,
+} from 'react-icons/ri'
+import { MdSearch, MdSearchOff } from 'react-icons/md'
+
+import { handleInfowoodsApiError } from '../../../utils/apiUtils'
+import { getChannels } from '../../../services/api/infowoods'
+
+import Empty from '../../../widgets/Empty'
+import Loading from '../../../widgets/Loading'
+
+import styles from './index.module.scss'
+function Channels(props) {
+  const { ctx, t, curLogin } = props
+  const router = useRouter()
+
+  function useMyChannels() {
+    const { data, error } = useSWR('my.channels', getChannels)
+    if (error) {
+      if (error) {
+        handleInfowoodsApiError(error, t, curLogin)
+      }
+    }
+    return {
+      data: data,
+      isLoading: !error && !data,
+      isError: error,
+    }
+  }
+
+  const myChannels = useMyChannels()
+
+  return (
+    <div className={styles.main}>
+      <p className={styles.sectionTitle}>
+        <TbTrees /> {t('my_channels')}
+      </p>
+      <div className={styles.cards}>
+        {myChannels.isLoading && <Loading size={'lg'} />}
+        {myChannels.data && (
+          <>
+            {myChannels.data?.channels?.length === 0 && (
+              <Empty
+                text={t('no_records')}
+                mainClass={styles.empty}
+                imageClass={styles.emptyImage}
+                textClass={styles.emptyText}
+              />
+            )}
+            {myChannels?.data?.channels?.length > 0 &&
+              myChannels.data.channels.map((item, index) => {
+                return (
+                  <Card
+                    key={index}
+                    className={styles.card}
+                    isPressable
+                    isHoverable
+                    variant="bordered"
+                    onPress={() => {
+                      const path = `channels/${item.id}`
+                      router.push(path)
+                    }}
+                  >
+                    <Card.Body>
+                      <Text b>{item.title}</Text>
+
+                      {item.description && (
+                        <Text className={styles.desc}>{item.description}</Text>
+                      )}
+                    </Card.Body>
+                    <Card.Footer css={{ justifyItems: 'flex-start' }}>
+                      <Row wrap="wrap" align="center" className={styles.status}>
+                        <Grid className={styles.state}>
+                          <RiMoneyDollarCircleLine />
+                          <span className={styles.value}>
+                            {item.price_per_info} NUT
+                          </span>
+                        </Grid>
+                        <Grid className={styles.state}>
+                          <span>
+                            <RiSearchLine />
+                          </span>
+                          <span className={styles.value}>
+                            {item.searchable ? (
+                              <RiCheckLine />
+                            ) : (
+                              <RiCloseLine />
+                            )}
+                          </span>
+                        </Grid>
+                        <Grid className={styles.state}>
+                          <RiUserFollowLine />
+                          <span className={styles.value}>
+                            {item.subscribers}
+                          </span>
+                        </Grid>
+                      </Row>
+                    </Card.Footer>
+                  </Card>
+                )
+              })}
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Channels
--- components/Home/index.js
+++ components/Home/index.js
@@ -1,302 +1,63 @@
 import { useEffect, useContext, useState } from 'react'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
-import { useForm, Controller } from 'react-hook-form'
 import dynamic from 'next/dynamic'
 import toast from 'react-hot-toast'
 
-import { ProfileContext } from '../../stores/useProfile'
+import { RiAddLine } from 'react-icons/ri'
 
-const OwlToast = dynamic(() => import('../../widgets/OwlToast'))
-const BottomSheet = dynamic(() => import('../../widgets/BottomSheet'))
-const Overlay = dynamic(() => import('../../widgets/Overlay'))
-const QrCodeSheet = dynamic(() => import('./QrCodeSheet'))
+import { getMixinContext } from '../../utils/pageUtil'
+import { CurrentLoginContext } from '../../contexts/currentLogin'
+const Toast = dynamic(() => import('../../widgets/Toast'))
 
-import Icon from '../../widgets/Icon'
-import Input from '../../widgets/Input'
-import TextArea from '../../widgets/TextArea'
+const ChannelCards = dynamic(() => import('./ChannelCards'))
+const ChannelCreate = dynamic(() => import('./ChannelCreate'))
 import Loading from '../../widgets/Loading'
-
-import { logout } from '../../utils/loginUtil'
-import { copyText } from '../../utils/copyUtil'
-import storageUtil from '../../utils/storageUtil'
-
-import {
-  getTopicsList,
-  triggerCreate,
-  modifyTopic,
-  getOrder,
-} from '../../services/api/amo'
-
 import styles from './index.module.scss'
 
 function Home() {
   const { t } = useTranslation('common')
-  const [state, dispatch] = useContext(ProfileContext)
-  const isLogin = state.isLogin
-  const { push } = useRouter()
+  const [curLogin, _] = useContext(CurrentLoginContext)
 
-  const [list, setList] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [showEdit, setShowEdit] = useState(false)
-  const [check, setCheck] = useState(false)
-  const [modifiedTopic, setModifiedTopic] = useState({})
-  const [payUrl, setPayUrl] = useState('')
-  const [orderId, setOrderId] = useState('')
-  const [intervalId, setIntervalId] = useState(null)
-
-  const [pushIdx, setPushIdx] = useState(null)
-  const [createLoading, setCreateLoading] = useState(false)
-  const [modifyLoading, setModifyLoading] = useState(false)
-
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm()
-
-  const getList = async () => {
-    setLoading(true)
-    try {
-      const data = await getTopicsList()
-      setLoading(false)
-      setList(data)
-    } catch (error) {
-      if (error?.action === 'logout') {
-        toast.error(t('auth_expire'))
-        setLoading(false)
-        logout(dispatch)
-        return
-      }
-    }
-  }
+  const [ctx, setCtx] = useState({})
+  const [showCreating, setShowCreating] = useState(false)
 
   useEffect(() => {
-    console.log('>>> home state:', state)
-    if (isLogin) {
-      getList()
-    } else if (isLogin === false) {
-      console.log('home not login')
-      setLoading(false)
-    }
-  }, [isLogin])
-
-  useEffect(() => {
-    if (check) {
-      const orderInterval = setInterval(async () => {
-        const res = await getOrder({ trace_id: orderId })
-        if (res?.topic_token) {
-          toast.success(t('create_success'))
-          setCheck(false)
-          setOrderId('')
-          getList()
-        }
-      }, 3000)
-      setIntervalId(orderInterval)
-    } else {
-      setOrderId('')
-      intervalId && clearInterval(intervalId)
-    }
-  }, [check])
-
-  const handleCreate = async () => {
-    setCreateLoading(true)
-    const res = await triggerCreate({ order_type: '1' })
-    setCreateLoading(false)
-    const payUrl =
-      'mixin://pay?' +
-      `recipient=${res?.recipient_id}` +
-      `&asset=${res?.asset_id}` +
-      `&amount=${res?.amount}` +
-      `&memo=${res?.memo}` +
-      `&trace=${res?.trace_id}`
-    setOrderId(res?.trace_id)
-    if (storageUtil.get('platform') === 'browser') {
-      setPayUrl(payUrl)
-    } else {
-      setCheck(true)
-      window.open(payUrl)
-    }
-  }
-
-  const handleEdit = (val) => {
-    setModifiedTopic(val)
-    setShowEdit(true)
-  }
-
-  const handleClose = () => {
-    setShowEdit(false)
-    setModifyLoading(false)
-    setModifiedTopic({})
-    reset()
-  }
-
-  const handleModify = async (data) => {
-    setModifyLoading(true)
-    console.log(data.title, data.description)
-    const res = await modifyTopic(modifiedTopic.topic_id, {
-      title: data.title,
-      description: data.description,
-    })
-    if (res?.title) {
-      setModifyLoading(false)
-      setShowEdit(false)
-      toast.success(t('modify_success'))
-      getList()
-    } else {
-      toast.error(t('modify_failed'))
-    }
-  }
+    const ctx = getMixinContext()
+    setCtx(ctx)
+  }, [])
 
   return (
     <div className={styles.main}>
-      {loading ? (
-        <Loading className={styles.homeLoading} />
-      ) : isLogin === false ? (
-        <div className={styles.ufo}>
-          <Icon type="ufo" />
-        </div>
-      ) : (
-        <div>
-          <p className={styles.mine}>我的</p>
+      {!ctx.platform && <Loading />}
+      {curLogin.token && (
+        <>
+          <ChannelCards ctx={ctx} t={t} curLogin={curLogin} />
 
-          {list.length > 0 &&
-            list.map((item, idx) => (
-              <div key={idx} className={styles.card}>
-                <p>
-                  <span>主题名：</span> {item.title}
-                </p>
-                <p>
-                  <span>简介：</span> {item.description || '--'}
-                </p>
-
-                <div className={styles.divider} />
-                <p className={styles.edit} onClick={() => handleEdit(item)}>
-                  <span>修改主题信息</span> <Icon type="edit" />
-                </p>
-                <p
-                  className={styles.copy}
-                  onClick={() => copyText(item.subscription_uri, toast, t)}
-                >
-                  订阅链接：
-                  <span>
-                    {item.subscription_uri} <Icon type="copy" />
-                  </span>
-                </p>
-
-                {pushIdx === idx ? (
-                  <Loading size={26} className={styles.pushLoading} />
-                ) : (
-                  <Icon
-                    type="add-circle"
-                    className={styles.post}
-                    onClick={() => {
-                      setPushIdx(idx)
-                      push(`/oak/${item.topic_id}`)
-                    }}
-                  />
-                )}
-              </div>
-            ))}
-
-          <div className={styles.create}>
-            👉
-            <span onClick={handleCreate}>创建新主题</span>
-            {createLoading && (
-              <Loading size={14} className={styles.createLoading} />
-            )}
+          <div
+            className={styles.channelAdd}
+            onClick={() => {
+              if (!curLogin.token) {
+                toast(t('login_first'), { icon: '💁' })
+                return
+              }
+              setShowCreating(true)
+            }}
+          >
+            <RiAddLine />
           </div>
-        </div>
+
+          <ChannelCreate
+            ctx={ctx}
+            t={t}
+            curLogin={curLogin}
+            showCreating={showCreating}
+            setShowCreating={setShowCreating}
+          />
+        </>
       )}
 
-      {/* Modify topic data */}
-      <BottomSheet
-        t={t}
-        show={showEdit}
-        withConfirm
-        confirmTitle="修改主题信息"
-        onClose={handleClose}
-        onCancel={handleClose}
-        onConfirm={handleSubmit((data) => handleModify(data))}
-      >
-        {modifyLoading ? (
-          <div className={styles.modifyLoading}>
-            <Loading />
-          </div>
-        ) : (
-          <div className={styles.sheet}>
-            <div className={styles.current}>
-              <p className={styles.title}>当前：</p>
-              <p>
-                <span>主题名：</span> {modifiedTopic.title}
-              </p>
-              <p>
-                <span>简介：</span> {modifiedTopic.description || '--'}
-              </p>
-            </div>
-
-            <div className={styles.modifyTo}>
-              <p className={styles.title}>改为：</p>
-              <form onSubmit={handleSubmit}>
-                <div className={styles.label}>
-                  <p>主题名：</p>
-                  <Controller
-                    name="title"
-                    control={control}
-                    render={({ field }) => (
-                      <Input className={styles.input} {...field} ref={null} />
-                    )}
-                    rules={{
-                      required: '*不能为空',
-                    }}
-                  />
-                  <p className={styles.error}>{errors?.title?.message}</p>
-                </div>
-                <div className={styles.label}>
-                  <p>简介：</p>
-                  <Controller
-                    name="description"
-                    control={control}
-                    render={({ field }) => (
-                      <TextArea
-                        className={styles.textArea}
-                        {...field}
-                        ref={null}
-                      />
-                    )}
-                    rules={{
-                      required: '*不能为空',
-                    }}
-                  />
-                  <p className={styles.error}>{errors?.description?.message}</p>
-                </div>
-              </form>
-            </div>
-          </div>
-        )}
-      </BottomSheet>
-
-      <QrCodeSheet
-        t={t}
-        show={payUrl}
-        id={payUrl}
-        onClose={() => setPayUrl('')}
-        onCancel={() => setPayUrl('')}
-        onConfirm={() => {
-          setPayUrl('')
-          setCheck(true)
-        }}
-      />
-
-      <Overlay
-        t={t}
-        desc={t('checking_pay')}
-        visible={check}
-        onCancel={() => setCheck(false)}
-      />
-
-      <OwlToast />
+      <Toast />
     </div>
   )
 }
